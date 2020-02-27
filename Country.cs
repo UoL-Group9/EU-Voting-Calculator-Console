57b6ded11fdacb64fa6f56f2aa3b0c67346e2f2d
Joshua James Richards <19705951@students.lincoln.ac.uk>
2020-02-27 01:33:09 +0000

    Added and partialy populated Voting class

---
+++ Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EU_Voting_Calculator
+{
+    class Country
+    {
+        double population;
+        int vote = 0;
+    }
+}
---
+++ VotingResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EU_Voting_Calculator
+{
+    class VotingResult
+    {
+        int memberstateyes = 0;
+        int memberstateno = 0;
+        int memberstateabs = 0;
+
+        double memberstatepop;
+
+        const int memberstateresult = 27;
+        const double memberpopresult = 65;
+    }
+}
